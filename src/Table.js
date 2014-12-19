bf7f3d824a9369889ef295aaa09a5cd1a0a14af0
Carlos Rocha <carlos.rocha58@gmail.com>
2014-12-19 15:49:33 -0600

    Merge table and table header

--- src/Table.js
+++ src/Table.js
@@ -1,11 +1,20 @@
 var React = require('react');
-var TableHeader = require('./TableHeader');
 
-var simpleGet = key => row => row[key];
+var simpleGet = key => data => data[key];
 var keyGetter = keys => data => keys.map(key => data[key]);
 
 var isEmpty = value => value === undefined || value === null || value === '';
 var isFunc = value => typeof value === 'function';
+
+var getSortClass =
+  (sortBy, prop) =>
+    sortBy.prop === prop ?
+      (sortBy.order === 'asc' ? 'sort-asc' : 'sort-desc') :
+      'sort-off';
+
+var getNextOrder =
+  (sortBy, prop) =>
+    sortBy.prop === prop && sortBy.order === 'asc' ? 'desc' : 'asc';
 
 var getCellValue =
   (col, row) =>
@@ -18,8 +27,6 @@ var getCellClass =
     col.prop && isEmpty(row[col.prop]) ? 'empty-cell' :
       isFunc(col.className) ? col.className(row[col.prop], row) :
       col.className;
-
-var emptyRow = <tr><td colSpan={100} className="text-center">No data</td></tr>;
 
 var Table = React.createClass({
 
@@ -50,17 +57,61 @@ var Table = React.createClass({
       React.PropTypes.array,
       React.PropTypes.object
     ])).isRequired,
-    buildRowOpts: React.PropTypes.func
+    buildRowOpts: React.PropTypes.func,
+    sortBy: React.PropTypes.shape({
+      prop: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number
+      ]),
+      order: React.PropTypes.oneOf([ 'asc', 'desc' ])
+    }),
+    onSort: React.PropTypes.func
   },
 
   getDefaultProps() {
-    return { buildRowOpts: () => ({}) };
+    return {
+      buildRowOpts: () => ({}),
+      sortBy: {}
+    };
+  },
+
+  componentDidMount() {
+    // If no width was specified, then set the width that the browser applied
+    // initially to avoid recalculating width between pages.
+    for (var i = 0; i < this.props.columns.length; i++) {
+      var thDom = this.refs[`th-${i}`].getDOMNode();
+      if (!thDom.style.width) {
+        thDom.style.width = `${thDom.offsetWidth}px`;
+      }
+    }
   },
 
   render() {
-    var { columns, keys, dataArray, buildRowOpts } = this.props;
+    var { columns, keys, buildRowOpts, sortBy, onSort } = this.props;
+
+    var headers = columns.map((col, idx) => {
+      var event, className = 'sort-disabled';
+      // Values that are not in the dataset are not sortable.
+      if (col.sortable !== false && col.prop !== undefined) {
+        var sortVal = { prop: col.prop, order: getNextOrder(sortBy, col.prop) };
+        event = onSort.bind(null, sortVal);
+        className = getSortClass(sortBy, col.prop);
+      }
+
+      return (
+        <th
+          ref={`th-${idx}`}
+          key={idx}
+          onClick={event}
+          style={{width: col.width}}>
+          {col.title}
+          <i className={`sort-icon ${className}`} />
+        </th>
+      );
+    });
+
     var getKeys = Array.isArray(keys) ? keyGetter(keys) : simpleGet(keys);
-    var rows = dataArray.map(
+    var rows = this.props.dataArray.map(
       row =>
         <tr key={getKeys(row)} {...buildRowOpts(row)}>
           {columns.map(
@@ -73,13 +124,14 @@ var Table = React.createClass({
 
     return (
       <table className={this.props.className}>
-        <TableHeader
-          columns={columns}
-          sortBy={this.props.sortBy}
-          onSort={this.props.onSort}
-        />
+        <thead>
+          <tr>
+            {headers}
+          </tr>
+        </thead>
         <tbody>
-          {rows.length ? rows : emptyRow}
+          {rows.length ? rows :
+            <tr><td colSpan={100} className="text-center">No data</td></tr>}
         </tbody>
       </table>
     );
