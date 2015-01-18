52e6068092ee4c23be689cfe00ce84c5195080f9
Carlos Rocha <carlos.rocha58@gmail.com>
2015-01-17 21:29:22 -0600

    Use cdn and rawgit instead of build

---
+++ build/pages.js
@@ -0,0 +1,34 @@
+var $__0=  ReactDataComponents,DataTable=$__0.DataTable;
+
+var byId = function(id)  {return document.getElementById(id);};
+
+function renderTable(data) {
+  var renderMapUrl =
+    function(val, row) 
+      {return React.createElement("a", {href: ("https://www.google.com/maps?q=" + row['LAT'] + "," + row['LON'])}, 
+        "Google Maps"
+      );};
+
+  var tableColumns = [
+    { title: 'Name', prop: 'NAME' },
+    { title: 'City', prop: 'CITY' },
+    { title: 'Street address', prop: 'STREET ADDRESS' },
+    { title: 'Phone', prop: 'PHONE NUMBER', defaultContent: '<no phone>' },
+    { title: 'Map', render: renderMapUrl, className: 'text-center' }
+  ];
+
+  return (
+    React.createElement(DataTable, {
+      keys: [ 'NAME', 'OUTLET TYPE', 'STREET ADDRESS'], 
+      columns: tableColumns, 
+      initialData: data, 
+      initialPageLength: 5, 
+      initialSortBy: { prop: 'CITY', order: 'desc'}, 
+      pageLengthOptions: [ 5, 20, 50]}
+    )
+  );
+}
+
+d3.csv('sample_data.csv', function(error, rows) {
+  React.render(renderTable(rows), byId('datatable'));
+});
---
+++ pages.js
@@ -0,0 +1,34 @@
+var {DataTable} = ReactDataComponents;
+
+var byId = id => document.getElementById(id);
+
+function renderTable(data) {
+  var renderMapUrl =
+    (val, row) =>
+      <a href={`https://www.google.com/maps?q=${row['LAT']},${row['LON']}`}>
+        Google Maps
+      </a>;
+
+  var tableColumns = [
+    { title: 'Name', prop: 'NAME' },
+    { title: 'City', prop: 'CITY' },
+    { title: 'Street address', prop: 'STREET ADDRESS' },
+    { title: 'Phone', prop: 'PHONE NUMBER', defaultContent: '<no phone>' },
+    { title: 'Map', render: renderMapUrl, className: 'text-center' }
+  ];
+
+  return (
+    <DataTable
+      keys={[ 'NAME', 'OUTLET TYPE', 'STREET ADDRESS' ]}
+      columns={tableColumns}
+      initialData={data}
+      initialPageLength={5}
+      initialSortBy={{ prop: 'CITY', order: 'desc' }}
+      pageLengthOptions={[ 5, 20, 50 ]}
+    />
+  );
+}
+
+d3.csv('sample_data.csv', function(error, rows) {
+  React.render(renderTable(rows), byId('datatable'));
+});
