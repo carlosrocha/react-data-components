f779c345b7274d3a312f03918da0889f19c8d45f
Carlos Rocha <carlos.rocha58@gmail.com>
2015-01-22 22:49:30 -0600

    Remove build

--- dist-modules/index.js
+++
@@ -1,7 +0,0 @@
-exports.DataTable = require('./DataTable');
-exports.Table = require('./Table');
-exports.Pagination = require('./Pagination');
-exports.SelectField = require('./SelectField');
-exports.SearchField = require('./SearchField');
-exports.DataMixin = require('./DataMixin');
-exports.utils = require('./utils');
