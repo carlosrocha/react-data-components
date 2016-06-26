f81ea3c2f245dd5f79cca05b7ba5f1eb2911e981
Carlos Rocha <carlos.rocha58@gmail.com>
2016-06-26 18:33:36 -0500

    Update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: 'babel-eslint',
+
+  plugins: [
+    'react',
+  ],
+
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    es6: true,
+  },
+
+  globals: {
+    jest: false,
+  },
+
+  ecmaFeatures: {
+    modules: true,
+    jsx: true,
+  },
+
+  rules: {
+    'strict': [ 2, 'global' ],
+    'quotes': [ 2, 'single' ],
+    'curly': [ 2, 'multi-line' ],
+    'eqeqeq': [ 2, 'smart' ],
+    'block-scoped-var': 2,
+    'comma-dangle': [ 2, 'always-multiline' ],
+    'no-underscore-dangle': 0,
+    'react/jsx-uses-react': 1,
+  },
+};
