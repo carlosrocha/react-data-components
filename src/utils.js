6bc62cada8338acb94d3f3f7f9a28f50684557b8
Cody Childers <cody_k_childers@homedepot.com>
2017-08-18 15:33:31 -0500

    debug updates

--- src/utils.js
+++ src/utils.js
@@ -29,24 +29,24 @@ export function sort({ prop, order }: SortBy, data: AppData) {
   console.log('all_are_numbers:');
   console.log(all_are_numbers);
 
-  // if (all_are_numbers) {
-  //   var orderByResults = orderBy(
-  //     data,
-  //     item => parseInt(item),
-  //     order === 'descending' ? 'desc' : 'asc',
-  //   );
-  // } else {
-  //   var orderByResults = orderBy(
-  //     data,
-  //     prop,
-  //     order === 'descending' ? 'desc' : 'asc',
-  //   );
-  // }
-  let orderByResults = orderBy(
-    data,
-    prop,
-    order === 'descending' ? 'desc' : 'asc',
-  );
+  if (all_are_numbers) {
+    var orderByResults = orderBy(
+      data,
+      item => parseInt(item),
+      order === 'descending' ? 'desc' : 'asc',
+    );
+  } else {
+    var orderByResults = orderBy(
+      data,
+      prop,
+      order === 'descending' ? 'desc' : 'asc',
+    );
+  }
+  // let orderByResults = orderBy(
+  //   data,
+  //   prop,
+  //   order === 'descending' ? 'desc' : 'asc',
+  // );
   console.log('orderByResults:');
   console.log(orderByResults);
   return orderByResults;
